b921d5686b43c3a07efae7469c7d8eda334e3707
SpectraS0ft <rnormurodov757@gmail.com>
2025-07-15 22:30:22 +0500

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -15,8 +15,8 @@ const accounts = [
     title: "PUBG Mobile Royal Pass",
     price: 9000,
     images: [
-      "https://via.placeholder.com/300x180?text=PUBG1",
-      "https://via.placeholder.com/300x180?text=PUBG2"
+      "https://i.ibb.co/wNQFKP3K/IMG-20250715-222728-618.jpg",
+      "https://i.ibb.co/Tq2rL7cJ/IMG-20250715-222723-032.jpg"
     ],
     description: "• M24 Skin\n• 85 Level\n• UZ Server\n• Facebook ulangan",
     telegram: "https://t.me/seller_pubg",
